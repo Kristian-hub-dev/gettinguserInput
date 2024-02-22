11598431c7b858d1f07556e8eb21a2ca76bcb9b9
Kristian-hub-dev <150884266+Kristian-hub-dev@users.noreply.github.com>
2024-02-23 07:42:05 +0800

    Add files via upload

---
+++ CalculatorBase.cs
@@ -0,0 +1,50 @@
+﻿namespace gettinguserInput
+{
+
+    internal class CalculatorBase
+    {
+        private int FirstNumber;
+        private int SecondNumber;
+
+        public CalculatorBase(int firstNumber, int secondNumber)
+        {
+            this.FirstNumber = firstNumber;
+            this.SecondNumber = secondNumber;
+        }
+
+        public void SetFirstNumber(int firstNumber)
+        {
+            this.FirstNumber = firstNumber;
+        }
+
+        public int GetFirstNumber()
+        {
+            return this.FirstNumber;
+        }
+
+        public int GetSecondNumber()
+        {
+            return this.SecondNumber;
+        }
+
+        public int GetSum()
+        {
+            return this.FirstNumber + this.SecondNumber;
+        }
+
+        public int GetProduct()
+        {
+            return this.FirstNumber * this.SecondNumber;
+        }
+
+        public int GetDifference()
+        {
+            return this.FirstNumber - this.SecondNumber;
+        }
+
+        public double GetQuotient()
+        {
+            return (double)this.FirstNumber / Math.Max(1, this.SecondNumber); // Use Math.Max to avoid division by zero
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using gettinguserInput;
+using System;
+       
+Console.WriteLine("Select the operation");
+Console.WriteLine("1 Press 1 for Addition");
+Console.WriteLine("2 Press 2 for Subtraction");
+Console.WriteLine("3 Press 3 for Multiplication");
+Console.WriteLine("4 Press 4 for Division");
+
+
+Console.WriteLine("Enter your choice:");
+int choice = int.Parse(Console.ReadLine());
+
+string FNumber, sNumber;
+
+
+Console.WriteLine("Enter the first number:");
+FNumber = Console.ReadLine();
+int fNum = Int32.Parse(FNumber);
+
+Console.WriteLine("Enter the second number:");
+sNumber = Console.ReadLine();
+int sNum = Int32.Parse(sNumber);
+
+CalculatorBase myCalculator = new CalculatorBase(fNum, sNum);
+
+        switch (choice)
+        {
+            case 1:
+                Console.WriteLine($"The sum of {fNum} and {sNum} is {myCalculator.GetSum()}");
+                break;
+            case 2:
+                Console.WriteLine($"The difference of {fNum} and {sNum} is {myCalculator.GetDifference()}");
+                break;
+            case 3:
+                Console.WriteLine($"The product of {fNum} and {sNum} is {myCalculator.GetProduct()}");
+                break;
+            case 4:
+                Console.WriteLine($"The quotient of {fNum} divided by {sNum} is {myCalculator.GetQuotient()}");
+                break;
+            default:
+                Console.WriteLine("Invalid choice! Please choose your choice (1-4).");
+                break;
+        }
+    
+
